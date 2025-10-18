050721a2d34e251c5d17cbf16035d05446ae29e6
darten125 <113637162+darten125@users.noreply.github.com>
2025-10-18 17:30:55 +0300

    14-meal-list-card: MealList UI implementation (#16)
    Co-authored-by: timatifey <timatifey.001@yandex.ru>

---
+++ app/src/main/java/dev/calorai/mobile/mainScreen/ui/utils/Utils.kt
@@ -0,0 +1,10 @@
+package dev.calorai.mobile.mainScreen.ui.utils
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.times
+
+internal fun calculateItemOffset(
+    itemSize: Dp,
+    itemOverlapOffset: Dp,
+    index: Int,
+): Dp = -((index + 1) * (itemSize - itemOverlapOffset))
---
+++ app/src/main/java/dev/calorai/mobile/ui/theme/Size.kt
@@ -0,0 +1,5 @@
+package dev.calorai.mobile.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+val circleMediumSize = 50.dp
