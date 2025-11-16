85cfdac8d9793c2a90f3cf0e171d7bb6fb29332c
MRT <mirkhat.tuyakov@mail.ru>
2025-11-16 18:25:42 +0300

    PrimaryTextField.kt was updated: - default value of placeholder was removed - code style fixes

--- app/src/main/java/dev/calorai/mobile/uikit/PrimaryTextField.kt
+++ app/src/main/java/dev/calorai/mobile/uikit/PrimaryTextField.kt
@@ -1,10 +1,7 @@
 package dev.calorai.mobile.uikit
 
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -14,21 +11,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import dev.calorai.mobile.R
 import dev.calorai.mobile.ui.theme.CalorAiTheme
 
 @Composable
 fun PrimaryTextField(
     value: String,
     onValueChange: (String) -> Unit,
-    placeholder: String = "",
+    placeholder: String,
     modifier: Modifier = Modifier,
-    keyboardOptions : KeyboardOptions = KeyboardOptions.Default,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
     visualTransformation: VisualTransformation = VisualTransformation.None
 ) {
     TextField(
@@ -37,20 +31,18 @@ fun PrimaryTextField(
         placeholder = {
             Text(
                 text = placeholder,
-                color = Color.Gray
-            ) },
+                color = MaterialTheme.colorScheme.onSurface
+            )
+        },
         singleLine = true,
         keyboardOptions = keyboardOptions,
         visualTransformation = visualTransformation,
         modifier = modifier
             .fillMaxWidth()
-            .padding(bottom = 6.dp)
             .shadow(elevation = 4.dp, shape = RoundedCornerShape(8.dp)),
         shape = MaterialTheme.shapes.medium,
         colors = TextFieldDefaults.colors(
-            //textColor = Color.Gray,
             disabledTextColor = Color.Transparent,
-            //backgroundColor = Color.White,
             focusedIndicatorColor = Color.Transparent,
             unfocusedIndicatorColor = Color.Transparent,
             disabledIndicatorColor = Color.Transparent
@@ -58,10 +50,10 @@ fun PrimaryTextField(
     )
 }
 
-@Preview(showBackground = true)
+@Preview()
 @Composable
 private fun PrimaryTextFieldPreview() {
     CalorAiTheme {
-        PrimaryTextField("", {})
+        PrimaryTextField("", {}, "placeholder")
     }
 }
