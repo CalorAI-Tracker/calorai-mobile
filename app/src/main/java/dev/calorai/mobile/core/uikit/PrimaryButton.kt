463dc0325611eeeff8200db650bd4eb9b814acc6
Mirkhat Tuyakov <mirkhat.tuyakov@gmail.com>
2025-11-26 23:15:38 +0300

    Feature/38 UI uikit button (#40)
    * Was added on AuthScreen:
    -Title and Description
    -Auth form
    -Divider
    -"Continue with Google" button
    
    * PrimaryButton.kt was created
    
    * PrimaryButton.kt was updated:
    - Default Text was added in Button
    - modifiers was added:
      - fillMaxWidth
      - height 56dp
    
    * PrimaryButton.kt was updated:
    - Default Text was deleted in Button
    - Preview fun was renamed
    
    * Revert unwanted changes
    
    * Move PrimaryButton to master's uikit directory
    
    * Revert unwanted changes in strings.xml
    
    * Text style fix
    
    * YAGNI 'content' code was deleted
    
    ---------
    
    Co-authored-by: MRT <mirkhat.tuyakov@mail.ru>

---
+++ app/src/main/java/dev/calorai/mobile/core/uikit/PrimaryButton.kt
@@ -0,0 +1,48 @@
+package dev.calorai.mobile.core.uikit
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun PrimaryButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+        .fillMaxWidth()
+        .height(56.dp),
+    text: String
+) {
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.secondary
+        ),
+        modifier = modifier,
+        shape = RoundedCornerShape(25)
+    ) {
+        Text(
+            text = text,
+            color = MaterialTheme.colorScheme.onSecondary,
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun PrimaryButtonPreview() {
+    CalorAiTheme {
+        PrimaryButton(text = "Далее",onClick = {})
+    }
+}
