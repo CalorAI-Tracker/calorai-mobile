bce8095aac67c352a24b9444ed0755b0644f777a
Mirkhat Tuyakov <mirkhat.tuyakov@gmail.com>
2025-11-26 23:16:30 +0300

    Feature/39 UI uikit textfield (#41)
    * Was added on AuthScreen:
    -Title and Description
    -Auth form
    -Divider
    -"Continue with Google" button
    
    * PrimaryButton.kt was created
    
    * PrimaryTextField.kt was created
    
    * PrimaryTextField.kt was updated:
    - default value of placeholder was removed
    - code style fixes
    
    * Revert unwanted changes
    
    * Move PrimaryTextField to master's uikit directory
    
    * Revert unwanted changes in strings.xml
    
    * TextField's modifier fix
    
    ---------
    
    Co-authored-by: MRT <mirkhat.tuyakov@mail.ru>

---
+++ app/src/main/java/dev/calorai/mobile/core/uikit/PrimaryTextField.kt
@@ -0,0 +1,61 @@
+package dev.calorai.mobile.core.uikit
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun PrimaryTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    placeholder: String,
+    modifier: Modifier = Modifier,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    visualTransformation: VisualTransformation = VisualTransformation.None
+) {
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        placeholder = {
+            Text(
+                text = placeholder,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+        },
+        singleLine = true,
+        keyboardOptions = keyboardOptions,
+        visualTransformation = visualTransformation,
+        modifier = modifier
+            .fillMaxWidth()
+            .shadow(3.dp, RoundedCornerShape(16.dp)),
+        shape = RoundedCornerShape(12.dp),
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = MaterialTheme.colorScheme.surface,
+            unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+            errorContainerColor = MaterialTheme.colorScheme.surface,
+            disabledTextColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent
+        )
+    )
+}
+
+@Preview()
+@Composable
+private fun PrimaryTextFieldPreview() {
+    CalorAiTheme {
+        PrimaryTextField("", {}, "placeholder")
+    }
+}
