8e73d942fa20d4b9c85c86bca0aee56e31a32460
LenaSpevak <elena.spevak2002@yandex.ru>
2025-12-07 18:38:01 +0300

    Добавлен UI экрана регистрации

--- app/src/main/java/dev/calorai/mobile/features/signUp/ui/SignUpRoot.kt
+++ app/src/main/java/dev/calorai/mobile/features/signUp/ui/SignUpRoot.kt
@@ -1,39 +1,28 @@
 package dev.calorai.mobile.features.signUp.ui
 
-import androidx.compose.foundation.background
+
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import dev.calorai.mobile.R
 import dev.calorai.mobile.core.uikit.CalorAiTheme
@@ -82,7 +71,8 @@ private fun SignUpScreen(
                 title = stringResource(R.string.signup_name),
                 placeholder = stringResource(R.string.signup_placeholder_name),
                 value = state.name,
-                onValueChange = { onEvent(SignUpUiEvent.NameEntered(it)) }
+                onValueChange = { onEvent(SignUpUiEvent.NameEntered(it)) },
+                keyboardType = KeyboardType.Text
             )
             Spacer(Modifier.size(20.dp))
             TextFieldWithTitle(
@@ -183,7 +173,7 @@ private fun TextFieldWithTitle(
 @Preview(showBackground = true)
 @Composable
 private fun SignUpScreenPreview() {
-    CalorAiTheme {
+    CalorAiTheme { 
         SignUpScreen(
             state = SignUpUiState(),
             onEvent = {}
