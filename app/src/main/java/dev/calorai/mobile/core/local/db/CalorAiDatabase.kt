1efff425ecb7dff8a304ff4457bbd4ea5254bd33
timatifey <timatifey.001@yandex.ru>
2026-04-25 03:58:19 +0300

    IngredientsEntity

--- app/src/main/java/dev/calorai/mobile/core/local/db/CalorAiDatabase.kt
+++ app/src/main/java/dev/calorai/mobile/core/local/db/CalorAiDatabase.kt
@@ -2,13 +2,24 @@ package dev.calorai.mobile.core.local.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import dev.calorai.mobile.features.meal.data.dao.DailyMealsDao
+import dev.calorai.mobile.features.meal.data.dao.IngredientsDao
+import dev.calorai.mobile.features.meal.data.entity.DailyMealsEntity
+import dev.calorai.mobile.features.meal.data.entity.IngredientsEntity
 import dev.calorai.mobile.features.profile.data.dao.UserDao
 import dev.calorai.mobile.features.profile.data.entity.UserEntity
-import dev.calorai.mobile.features.meal.data.dao.DailyMealsDao
-import dev.calorai.mobile.features.meal.data.entity.DailyMealsEntity
 
-@Database(entities = [UserEntity::class, DailyMealsEntity::class], version = 1)
-abstract class CalorAiDatabase: RoomDatabase() {
+@Database(
+    entities = [
+        UserEntity::class,
+        DailyMealsEntity::class,
+        IngredientsEntity::class,
+    ],
+    version = 2,
+    exportSchema = false,
+)
+abstract class CalorAiDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
     abstract fun dailyMealDao(): DailyMealsDao
+    abstract fun ingredientsDao(): IngredientsDao
 }
